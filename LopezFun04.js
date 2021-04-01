2f672a506d82f59087f4e67ec42a3101da17551e
Mati240594 <80931005+Mati240594@users.noreply.github.com>
2021-04-01 18:35:57 -0300

    Add files via upload

---
+++ LopezFun04.js
@@ -0,0 +1,11 @@
+const suma =(a,b) => a + b;
+let precioproducto1 = parseInt(prompt("Ingresa costo de un producto"));
+let precioproducto2= parseInt(prompt("Ingresa costo de un producto"));
+const IVA1 = x => precioproducto1* 0.21;
+const IVA2 = y => precioproducto2*0.21;
+let precioventa = suma(suma(precioproducto1,IVA1),suma(precioproducto2,IVA2));
+console.log(precioventa);
+
+
+
+
